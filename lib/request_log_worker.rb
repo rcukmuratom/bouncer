c46fc49de18c1665a45baf124524be6ed0f5929a
Michael Wawra <michael@dxw.com>
2016-05-17 11:33:36 +0100

    (feature) Worker to log requests
    This is a very simple worker that asynchronously logs requests into the
    database, so they can be floded up into the format `Transition` expectes
    later.
    
    NB: We **could** have done this by writing Redis keys, but based on the
    volume or traffic we anticiapte for this, this should be sufficient for
    now.

---
+++ lib/request_log_worker.rb
@@ -0,0 +1,10 @@
+class RequestLogWorker
+  include Sidekiq::Worker
+
+  sidekiq_options :queue => :request_log
+
+  def perform(params_json)
+    params = JSON.parse(params_json, symbolize_names: true)
+    Requests.create(params)
+  end
+end
